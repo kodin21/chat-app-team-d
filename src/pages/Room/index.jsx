4163bbbab49a0df3f14ee8b01239ff4afe6ed3d9
Merve Bacak <nmerveb@gmail.com>
2021-08-11 12:39:38 +0300

    srollbar added

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 import styles from "./Room.module.css";
 
 function Room() {
   return (
-    <div className={styles.App}>
-      <div className={styles["Left-Sidebar"]}>
-        <div className={styles.LogoContainer}>
-          <div className={styles.Logo}>Logo</div>
-        </div>
-        <div className={styles.ChannelsList}>
-          <div className={styles.ChannelsListContainer}>
-            <div className={styles.Channel}>
-              <span className={styles.ChannelTitle}>Title</span>
-              <div className={styles.ChannelUserCount}>22 </div>
-              <span className={styles.ChannelJoinButton}>Join</span>
+    <div className={styles.AppBackground}>
+      <div className={styles.App}>
+        <div className={styles["Left-Sidebar"]}>
+          <div className={styles.LogoContainer}>
+            <div className={styles.Logo}>Logo</div>
+          </div>
+          <div className={styles.ChannelsList}>
+            <div className={styles.ChannelsListContainer}>
+              <div className={styles.Channel}>
+                <span className={styles.ChannelTitle}>Title</span>
+                <div className={styles.ChannelUserCount}>22 </div>
+                <span className={styles.ChannelJoinButton}>Join</span>
+              </div>
             </div>
           </div>
+          <div className={styles.AddNetwork}>AddNetwork</div>
         </div>
-        <div className={styles.AddNetwork}>AddNetwork</div>
-      </div>  
-      <div className={styles.Room}>
+        </div>  
+        <div className={styles.Room}>
         <div className={styles["Room-Title"]}>{"{channel.title}"}</div>
         <div className={styles.Main}>
           <div className={styles.MessageList}></div>
@@ -32,22 +34,23 @@ function Room() {
                 <div className={styles.TimeSpan}>Time Span</div>
               </div>
                  message
+                </div>
+              </div>
+            </div>
+            <div className={styles["Online-Users"]}>
+              <div className={styles.OnlineUsersTitle}>Online Users</div>
+              <div className={styles.UserList}>
+                <div className={styles.UserListContainer}>
+                  <div className={styles.User}>User</div>
+                </div>
               </div>
             </div>
           </div>
-          <div className={styles["Online-Users"]}>
-             <div className={styles.OnlineUsersTitle}>Online Users</div>
-             <div className={styles.UserList}>
-              <div className={styles.UserListContainer}>
-                <div className={styles.User}>User</div>
-              </div>
-             </div>
-          </div>
+          <div className={styles["Message-Input"]}>Message Input</div>
         </div>
-        <div className={styles["Message-Input"]}>Message Input</div>
       </div>
-    </div>  
-    )
-  }
+    </div>
+  );
+}
 
-export default Room
+export default Room;
