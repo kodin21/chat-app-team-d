0e345406fb4f31d26fc5b01b81cf4930278ef69f
movwf <movwf@protonmail.com>
2021-08-11 11:40:41 +0300

    Feat: Page routes are initiated.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-import Login from './pages/Login';
-import Room from './pages/Room';
+import { useState } from "react";
+import "./App.css";
+import Room from "./pages/Room";
+import Login from "./pages/Login";
+import { Redirect, Router } from "@reach/router";
 
 function App() {
+  const [username, setUsername] = useState(null);
+
   return (
     <div className="AppBackground">
-     {<Room />}
-     {false&&<Login/>}
+      <Router>
+        {/* Is logged in (Look for localStorage) ? */}
+        {username ? (
+          <Redirect from="/" to="rooms" noThrow />
+        ) : (
+          <Redirect from="/" to="login" noThrow />
+        )}
+        {/* Page routes */}
+        <Login path="login" {...{ setUsername }} />
+        {/* General room */}
+        <Room path="rooms" />
+        {/* Seperate rooms */}
+        <Room path="room/:roomId" />
+      </Router>
     </div>
   );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from "react";
 import styles from "./Login.module.css";
+import { navigate } from "@reach/router";
 
-function Login() {
+function Login({ setUsername }) {
   return (
     <div className={styles.Login}>
       <div className={styles.LoginBox}>
         <div className={styles.LoginBoxTitle}>{"{box.title}"}</div>
         <div className={styles.LoginBoxMain}>
-        <span>Kullancı Adı</span>
-        <input name="username"  type="text"/>
-        <button type="button"> Giriş</button>
+          <span>Kullancı Adı</span>
+          <input name="username" type="text" />
+          <button
+            type="button"
+            onClick={() => {
+              setUsername("test");
+              navigate("rooms");
+            }}
+          >
+            Giriş
+          </button>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
