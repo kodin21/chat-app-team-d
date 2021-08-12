b132ea55ed7a282655067a5d0f62b26e3d4aebce
Merve Bacak <nmerveb@gmail.com>
2021-08-12 13:15:30 +0300

    ui:Room component atomized

--- src/components/Room/index.jsx
+++ src/components/Room/index.jsx
@@ -1,50 +1,42 @@
-import React from 'react'
-import {useParams} from "react-router-dom";
-import styles from "./Room.module.css";
-import MessageInput from "./MessageInput";
-import RoomHeader from "./RoomHeader";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Room.module.css';
+import OnlineUsersCount from './OnlineUsersCount';
+import MessageInput from './MessageInput';
+import OnlineUser from './OnlineUser';
+import RoomHeader from './RoomHeader';
+import Message from './Message';
 
 function Room() {
-  const {roomId} = useParams();
+  const { roomId } = useParams();
   return (
     <div className={styles.Room}>
-    <RoomHeader roomTitle={roomId} />
-    <div className={styles.Main}>
-      <div className={styles.Messages}>
-        <div className={styles.MessageList}>
-          <div className={styles.Message}>
-            <div className={styles.UserAvatarContainer}>
-              <div className={styles.UserAvatar}>
-                A
-              </div>
-            </div>
-            <div className={styles.MessageBody}>
-              <div className={styles.MessageTitle}>
-                <span className={styles.Username}>Username</span>
-                <span className={styles.TimeStamp}>22&apos;Aug - 12:50</span>
-              </div>
-              <span className={styles.MessageContent}>
-                Message
-              </span>
-            </div>
+      <RoomHeader title={roomId} />
+      <div className={styles.Main}>
+        <div className={styles.Messages}>
+          <div className={styles.MessageList}>
+            <Message
+              {...{
+                userName: 'Test',
+                userColor: 'plum',
+                messageTime: "22'Aug 11:00",
+                messageContent: 'Hello World!',
+              }}
+            />
           </div>
-        </div>              
-      </div>            
-      <div className={styles["Online-Users"]}>
-        <div className={styles.OnlineUsersTitle}>22 people here</div>
-        <div className={styles.UserListContainer}>
-          <div className={styles.UserList}>
-            <div className={styles.User}>
-              <span className={styles.UserPointer}>-</span>
-              <span className={styles.Username}>User - 1</span>
+        </div>
+        <div className={styles['Online-Users']}>
+          <OnlineUsersCount count={22} />
+          <div className={styles.UserListContainer}>
+            <div className={styles.UserList}>
+              <OnlineUser {...{userName: "User - 1",userColor: "plum"}} />
             </div>
           </div>
         </div>
       </div>
+      <MessageInput />
     </div>
-    <MessageInput />
-  </div>
-  )
+  );
 }
 
-export default Room
+export default Room;
