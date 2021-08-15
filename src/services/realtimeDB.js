0ce81e90d398bc008c00aa85ce495879c6574e33
movwf <movwf@protonmail.com>
2021-08-15 06:13:56 +0300

    Service: Firebase realtime database functions created.

---
+++ src/services/realtimeDB.js
@@ -0,0 +1,25 @@
+import firebase from './firebase';
+import 'firebase/storage';
+
+const db = firebase.database();
+
+export function ConnectUser(userObject, roomID){
+  const connectionRef = db.ref(`/rooms/${roomID}`);
+  // Create user object for connection
+  connectionRef.push(userObject);
+  // Listen disconnect and pull off from tree
+  connectionRef.onDisconnect().remove();
+
+  return connectionRef;
+}
+
+export function SubscribeConnectedUsersData(setter){
+  const roomsRef = db.ref(`/rooms`);
+  // Subscribe to rooms data
+  roomsRef.on('value', (snapshot)=>{
+    const data = snapshot.val(); // Rooms' object or array
+
+    setter(data); // Callback for use
+  })
+
+}
