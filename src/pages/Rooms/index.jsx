6a0f49e4d7d189b5a02aab32b3ee9f4911ad0e54
movwf <movwf@protonmail.com>
2021-08-15 06:58:20 +0300

    Feat: Subscribed to connected users data.

--- src/pages/Rooms/index.jsx
+++ src/pages/Rooms/index.jsx
@@ -4,24 +4,28 @@ import { SubscribeRoomsData } from '../../services/fireStore';
 import styles from './Rooms.module.css';
 import LeftSideBar from '../../components/LeftSideBar';
 import Room from '../../components/Room';
+import { SubscribeConnectedUsersData } from '../../services/realtimeDB';
 
 function Rooms({clientUser}) {
   const match = useRouteMatch();
-  // eslint-disable-next-line no-unused-vars
   const [roomsData, setRoomsData] = useState([]);
+  const [connectedUsers, setConnectedUsers] = useState({});
 
   useEffect(() => {
     // Initiate rooms data subscription
     SubscribeRoomsData(setRoomsData);
+
+    // Initiate conencted users data for rooms
+    SubscribeConnectedUsersData(setConnectedUsers)
   }, []);
 
   return (
     <div className="AppBackground">
       <div className={styles.App}>
-        <LeftSideBar {...{ roomsData, clientUser }} />
+        <LeftSideBar {...{ roomsData, clientUser, connectedUsers }} />
         <Switch>
           <Route path={`${match.path}/:roomId`}>
-            <Room {...{ roomsData, clientUser }} />
+            <Room {...{ roomsData, clientUser, connectedUsers }} />
           </Route>
         </Switch>
       </div>
