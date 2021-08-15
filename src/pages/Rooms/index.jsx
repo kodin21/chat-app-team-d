370590b00dfba0aabdd9fb87b63cc0ddc8d68d97
movwf <movwf@protonmail.com>
2021-08-15 05:59:57 +0300

    Fix: App crasher subscription disabled.

--- src/pages/Rooms/index.jsx
+++ src/pages/Rooms/index.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import { SubscribeRoomsData } from '../../services/fireStore';
+// import { SubscribeRoomsData } from '../../services/fireStore';
 import styles from './Rooms.module.css';
 import LeftSideBar from '../../components/LeftSideBar';
 import Room from '../../components/Room';
 
 function Rooms({userName}) {
   const match = useRouteMatch();
+  // eslint-disable-next-line no-unused-vars
   const [roomsData, setRoomsData] = useState([]);
 
   useEffect(() => {
     // Initiate rooms data subscription
-    SubscribeRoomsData(setRoomsData);
+    // SubscribeRoomsData(setRoomsData);
   }, []);
 
   return (
